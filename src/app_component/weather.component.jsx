f64fcde375d458325192904dc02bcbef46565b92
suhas <suhas.messi7@gmail.com>
2019-12-22 16:03:10 +0530

    add UI

--- src/app_component/weather.component.jsx
+++ src/app_component/weather.component.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
 
-const Weather = () => {
+const Weather = (props) => {
     return(
         <div className="container">
-            <h1>Weather App</h1>
+            <div className="cards">
+                <h1>
+                {props.city},{props.country}
+                </h1>
+                <h5 className="py-4">
+                    <i className={`wi ${props.weatherIcon} display-1`}></i>
+                </h5>
+                <h1 className="py-2">{props.temp_celsius}&deg;</h1>
+                { /*Show Max and Min Temp */}
+                {minmaxTemp(props.temp_min, props.temp_max)}
+                <h4 className="py-3">{props.description}</h4>
+            </div>
         </div>
+    )
+}
+
+function minmaxTemp(min,max){
+    return(
+        <h3>
+            <span className="px-4">{min}&deg;</span>
+            <span className="px-4">{max}&deg;</span>
+        </h3>
     )
 }
 
