97fb1fcd574bb6f84a0f49afbf76d71a1a263407
suhas <suhas.messi7@gmail.com>
2019-12-22 17:30:24 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import './App.css';
+
 import Weather from './app_component/weather.component'
+import Form from './app_component/form.component'
+
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'weather-icons/css/weather-icons.css'
 
@@ -21,7 +24,6 @@ class App extends React.Component{
       description: "",
       error: false
     };
-    this.getWeather();
 
     this.weatherIcon ={
       Thunderstorm : "wi-thunderstorm",
@@ -65,31 +67,42 @@ class App extends React.Component{
           this.setState({icon:this.weatherIcon.Clouds})
           break;
         default:
-          this.setState({icon:this.weatherIcon.Clouds})
+          this.setState({icon:this.weatherIcon.Clear})
     }
   }
 
-  getWeather = async ()=>{
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${Api_key}`);
+  getWeather = async (event)=>{
+    event.preventDefault();
+    const city = event.target.elements.city.value;  
+    const country = event.target.elements.country.value;  
+    
+    if(city && country){
+      const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${Api_key}`);
    
     const response = await api_call.json();
     console.log(response);
 
     this.setState({
-      city:response.name,
-      country: response.sys.country,
+      city:`${response.name},${response.sys.country}`,
       celsius: this.calCelsius(response.main.temp),
       temp_max: this.calCelsius(response.main.temp_max),
       temp_min: this.calCelsius(response.main.temp_min),
       description: response.weather[0].description,
+      error:false
     });
     this.getWeatherIcon(this.weatherIcon, response.weather[0].id);
+    }
+    else{
+      this.setState({error:true})
+    }
+
   };
 
   state ={}
   render(){
     return(
       <div className="App">
+        <Form loadWeather={this.getWeather} error={this.state.error}/>
         <Weather 
           city={this.state.city} 
           country={this.state.country} 
--- src/app_component/weather.component.jsx
+++ src/app_component/weather.component.jsx
@@ -3,14 +3,18 @@ import React from 'react'
 const Weather = (props) => {
     return(
         <div className="container">
-            <div className="cards">
+            <div className="cards pt-4">
                 <h1>
-                {props.city},{props.country}
+                {props.city}
                 </h1>
                 <h5 className="py-4">
                     <i className={`wi ${props.weatherIcon} display-1`}></i>
                 </h5>
-                <h1 className="py-2">{props.temp_celsius}&deg;</h1>
+
+                {props.temp_celsius?(
+                    <h1 className="py-2">{props.temp_celsius}&deg;</h1>
+                ):null}
+
                 { /*Show Max and Min Temp */}
                 {minmaxTemp(props.temp_min, props.temp_max)}
                 <h4 className="py-3">{props.description}</h4>
@@ -20,12 +24,14 @@ const Weather = (props) => {
 }
 
 function minmaxTemp(min,max){
-    return(
-        <h3>
-            <span className="px-4">{min}&deg;</span>
-            <span className="px-4">{max}&deg;</span>
-        </h3>
-    )
+    if(min && max) {
+        return(
+            <h3>
+                <span className="px-4">{min}&deg;</span>
+                <span className="px-4">{max}&deg;</span>
+            </h3>
+        )
+    }
 }
 
 export default Weather
